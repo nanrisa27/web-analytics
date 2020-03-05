ecced31cf4a54e808f781e3793a03af81364c038
Minyoi87 <minyoi2710@gmail.com>
2020-03-05 14:15:53 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('that was it');
+
+var a = 1;
+var b = 2;
+console.error(a + b);
+
+if(a>b){
+    console.warm(c);
+}
+if(a<b)
+{// do the following if a is lesser than b
+    console.log('ais lesser than b');console.log(a+'is greater than'+b);
+} else{ // do the following things when otherwise
+    console.log('a is not greater than b');
+}
+if(a=1){console.log('a is exactly 1');
+}
